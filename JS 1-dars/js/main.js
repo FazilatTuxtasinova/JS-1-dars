0ccf03a7809e35b79c52adf37adb61f2a4d31185
FazilatTuxtasinova <147168697+FazilatTuxtasinova@users.noreply.github.com>
2023-10-23 19:57:44 +0500

    JS 1-dars

---
+++ JS 1-dars/js/main.js
@@ -0,0 +1,37 @@
+/*console.log("Men console oynasida ko'rinaman!");*/
+/*document.write("Men body oynasida ko'rinaman!");*/
+/*alert("Saytimizga xush kelisiz!");*/
+/*console.log(10+10);*/
+
+/*let x = 5;*/
+/*let y = 2;*/
+/*let z = 10;*/
+/*x = 7;*/
+/*let x =7, y = 5, z = 10*/
+/*document.write(x+y+z);*/
+
+/*let salom = "Assalomu alaykum!"; */
+/*let rang = "#f1f1f1";*/
+/*document.write(rang);*/
+
+/*let x = 5;*/
+/*let y = "JavaScript";*/
+/*document.write(typeof y);*/
+
+let x = 3;
+/*x+=22;*/
+/*x-=2;*/
+/*x*=7;*/
+/*x/=5;*/
+/*let qoldiq = x%5;*/
+/*let daraja  = x**2;*/
+--x;
+--x;
+--x;
+
+document.write(x);
+
+
+
+
+
